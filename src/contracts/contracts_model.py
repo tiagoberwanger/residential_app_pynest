dabbdd776b3c67278202c6decf40c20cd7739294
Tiago Berwanger <tiago.berwanger@mercos.com>
2024-01-08 00:07:52 -0300

    adiciona validador

--- src/contracts/contracts_model.py
+++ src/contracts/contracts_model.py
@@ -1,7 +1,9 @@
 from decimal import Decimal
 from datetime import date, datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
+from src.rooms.rooms_service import RoomsService
+from src.users.users_service import UsersService
 from src.utils.enums import StatusEnum
 
 
@@ -12,6 +14,20 @@ class ContractPostSchema(BaseModel):
     end_date: date = Field(default='2024-01-01')
     rent_value: Decimal = Field(default=Decimal(500))
     status: StatusEnum = Field(default=StatusEnum.ACTIVE)
+
+    @validator('user_id')
+    def validate_user(cls, user_id: int):
+        user = UsersService().get_user_by_id(user_id)
+        if not user:
+            raise ValueError(f"User {user_id} not found")
+        return user_id
+
+    @validator('room_id')
+    def validate_room(cls, room_id: int):
+        room = RoomsService().get_room_by_id(room_id)
+        if not room:
+            raise ValueError(f"Room {room_id} not found")
+        return room_id
 
     class ConfigDict:
         from_attributes = True
